0229bc3b0ac886a8aa6db0487c20cc4b4e58a8bf
slhscs217 <ethanlanting@gmail.com>
2021-12-02 08:03:59 -0600

    Changes by SLHSCS203

--- src/graphics/panels/HelpPanel.java
+++ src/graphics/panels/HelpPanel.java
@@ -1,8 +1,11 @@
 package graphics.panels;
 
 import graphics.Window;
+import graphics.util.ButtonFactory;
+import graphics.util.CenteredPosition;
 import graphics.util.ImageCreate;
 import graphics.util.ImageScaler;
+import java.awt.*;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -11,6 +14,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class HelpPanel extends JPanel {
+    private final JButton exit;
+    private Container instr;
+    private JTextArea area;
+
+    public HelpPanel () {
+        setLayout(null);
+        JLabel title = null;
+        add(title);
+        exit = ButtonFactory.create("/images/buttons/title/exit.png", "/images/buttons/title/exit-pressed.png", 200, 100);
+        setActionListeners();
+
+        int width = exit.getPreferredSize().width;
+        int height = exit.getPreferredSize().height;
+        exit.setBounds(800, 800, width, height);
+        add(exit);
+
+
+
+
+
+    }
+
+    private void setActionListeners() {
+        exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Pressed exit");
+                RootPanel.switchPanel("Title");
+            }
+        });
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
